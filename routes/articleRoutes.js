cf1f9f99b1c101e6571874dad54317e23011c632
Erwin Hartanto <erwinhaa25@gmail.com>
2021-02-25 14:11:31 +0700

    Removed createArticle handler

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { getArticleDetail, getCreateArticle } = require('../controllers/articleController')
 
-router.get('/create-article', getCreateArticle);
+
 router.get('/article-detail', getArticleDetail);
 
 module.exports = router;
